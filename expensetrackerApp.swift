c3a111be387f315e6edd1505c1a194335aabe6e0
Jauneetsingh <jauneet.singh@gmail.com>
2025-08-05 12:29:04 +0700

    Add expensetrackerApp.swift 1754371744058

---
+++ expensetrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  expensetrackerApp.swift
+//  expensetracker
+//
+//  Created by jauneet singh on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct expensetrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
